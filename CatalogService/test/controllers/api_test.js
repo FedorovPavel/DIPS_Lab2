f1ca256f9b4969ed430d72304e8097030357541d
Pavel <forsach95@mail.ru>
2017-11-01 16:27:11 +0300

    Добавлены тесты для апи каталога

--- CatalogService/test/controllers/api_test.js
+++ CatalogService/test/controllers/api_test.js
@@ -12,8 +12,6 @@ chai.use(chaiHttp);
 
 describe('api cars controller', function(){
 
-    
-
     describe('Get cars 0 - 20', function(){
         it('Good request /catalog/get_cars/page/*/count/*', function(done){
             chai.request(server)
@@ -27,5 +25,129 @@ describe('api cars controller', function(){
         });
     });
 
-    
+    describe('Get cars 0 - 20', function(){
+        it('Bad request /catalog/get_cars/page/*/count/* without page', function(done){
+            chai.request(server)
+            .get('/catalog/get_cars/page/count/20')
+            .end(function(err, res) {
+                res.should.have.status(404);
+                res.body.should.be.a('object');
+                done();
+            });
+        });
+    });
+
+    describe('Get cars 0 - 20', function(){
+        it('Bad request /catalog/get_cars/page/*/count/* without count', function(done){
+            chai.request(server)
+            .get('/catalog/get_cars/page/10/count/')
+            .end(function(err, res) {
+                res.should.have.status(404);
+                res.body.should.be.a('object');
+                done();
+            });
+        });
+    });
+
+    describe('Get cars 0 - 20', function(){
+        it('Bad request /catalog/get_cars/page/*/count/* with bad parametr page', function(done){
+            chai.request(server)
+            .get('/catalog/get_cars/page/bad_parametr/count/20')
+            .end(function(err, res) {
+                res.should.have.status(400);
+                res.type.should.to.be.a('string');
+                res.text.should.eql('Bad request');
+                done();
+            });
+        });
+    });
+
+    describe('Get cars 0 - 20', function(){
+        it('Bad request /catalog/get_cars/page/*/count/* with bad parametr count', function(done){
+            chai.request(server)
+            .get('/catalog/get_cars/page/0/count/bad_parametr')
+            .end(function(err, res) {
+                res.should.have.status(400);
+                res.type.should.to.be.a('string');
+                res.text.should.eql('Bad request');
+                done();
+            });
+        });
+    });
+
+    describe('Get cars 0 - 20', function(){
+        it('Bad request /catalog/get_cars/page/*/count/* with bad parametr count', function(done){
+            chai.request(server)
+            .get('/catalog/get_cars/page/0/count/bad_parametr')
+            .end(function(err, res) {
+                res.should.have.status(400);
+                res.type.should.to.be.a('string');
+                res.text.should.eql('Bad request');
+                done();
+            });
+        });
+    });
+
+    describe('Get cars 0 - 20', function(){
+        it('Bad request /catalog/get_cars/page/*/count/* with negative count', function(done){
+            chai.request(server)
+            .get('/catalog/get_cars/page/0/count/-20')
+            .end(function(err, res) {
+                res.should.have.status(400);
+                res.type.should.to.be.a('string');
+                res.text.should.eql('Bad request');
+                done();
+            });
+        });
+    });
+
+    describe('Get cars 0 - 20', function(){
+        it('Bad request /catalog/get_cars/page/*/count/* with negative page', function(done){
+            chai.request(server)
+            .get('/catalog/get_cars/page/-20/count/20')
+            .end(function(err, res) {
+                res.should.have.status(400);
+                res.type.should.to.be.a('string');
+                res.text.should.eql('Bad request');
+                done();
+            });
+        });
+    });
+
+    describe('Get car with id', function(){
+        it('Good request /catalog/get_car/:id', function(done){
+            chai.request(server)
+            .get('/catalog/get_car/59f634f54929021fa8251633')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                done();
+            });
+        });
+    });
+
+    describe('Get car with id', function(){
+        it('Bad request /catalog/get_car/:id without id', function(done){
+            chai.request(server)
+            .get('/catalog/get_car/')
+            .end(function(err, res) {
+                res.should.have.status(404);
+                res.body.should.be.a('object');
+                done();
+            });
+        });
+    });
+
+    describe('Get car with id', function(){
+        it('Bad request /catalog/get_car/:id with undefined id', function(done){
+            chai.request(server)
+            .get('/catalog/get_car/undefined')
+            .end(function(err, res) {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.status.should.eql('Error');
+                done();
+            });
+        });
+    });
 });
