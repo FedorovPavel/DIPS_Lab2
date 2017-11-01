b849199f782ee43addd941d6c7cc9010ab7133c0
Pavel <forsach95@mail.ru>
2017-11-01 17:53:12 +0300

    Добавлены 2 обработчика 1) Получение машин с пагинацией 2) Получение машины по ID Оба обработчика используют bus для связи с сервисом Catalog

--- AggregationService/app/controllers/aggregation_api.js
+++ AggregationService/app/controllers/aggregation_api.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    router = express.Router();
+    router  = express.Router(),
+    bus     = require('./../request_send_function/bus');
 
 module.exports = function (app) {
   app.use('/', router);
@@ -9,4 +10,36 @@ router.get('/', function (req, res, next) {
   res.render('index', {
       title: 'Generator-Express MVC'
   });
+});
+
+// Get any cars
+router.get('/catalog/cars/:page/:count', function(req, res, next){
+  const page  = parseInt(req.params.page);
+  const count = parseInt(req.params.count);
+  if(page < 0 || isNaN(page) || count < 0 || isNaN(count)){
+    res.send('Bad request');
+  } else {
+    bus.getCars(page, count, function(err, statusCode, responseText){
+      if (err)
+        return next(err);
+      else {
+        res.status(statusCode).send(responseText);
+      }
+    });
+  }
+});
+
+//  Get car by ID
+router.get('/catalog/car/:id', function(req, res, next){
+  const id = req.params.id;
+  if (id.length == 0 || typeof(id) == 'undefined' || !id){
+    res.send('Bad request');
+  } else {
+    bus.getCar(id, function(err, statusCode, responseText){
+      if (err)
+        return next(err);
+      else 
+        res.status(statusCode).send(responseText);
+    });
+  }
 });
