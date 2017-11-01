89ecc6e3df1e499f43a48260a967e1e3d3e63c55
Pavel <forsach95@mail.ru>
2017-11-01 18:02:19 +0300

    Тест для проверки связи Aggregation и Catalog services при правильном запросе с отправленного на агрегатор Для метода соединения getCars

--- AggregationService/test/unit/catalog_test.js
+++ AggregationService/test/unit/catalog_test.js
@@ -7,3 +7,19 @@ var chai        = require('chai'),
 
 chai.use(chaiHttp);
 
+describe('Unit test with CatalogService', function(){
+    describe('Get cars', function(){
+        describe('Good request', function(){
+            it('Good request /catalog/cars/*/*', function(done){
+                chai.request(server)
+                .get('/catalog/get_cars/page/0/count/20')
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(20);
+                    done();
+                });
+            });
+        });
+    });
+});
