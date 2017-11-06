a8b4754954dec9a6313d97563660d0c6ef728e26
Pavel <forsach95@mail.ru>
2017-11-06 17:11:13 +0300

    файл для проведения тестов на данном сервисе

---
+++ OrderingService/test/api/api.js
@@ -0,0 +1,10 @@
+process.env.NODE_ENV = 'development';
+
+var mongoose    = require('mongoose');
+
+var chai        = require('chai'),
+    chaiHttp    = require('chai-http'),
+    server      = require('./../../app.js'),
+    should      = chai.should();
+
+chai.use(chaiHttp);
