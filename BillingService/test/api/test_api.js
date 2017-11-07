2fc2c73788472a34a134efd375d5a5c82a7803e3
Pavel <forsach95@mail.ru>
2017-11-07 22:37:00 +0300

    Правка подключаемых модулей

--- BillingService/test/api/test_api.js
+++ BillingService/test/api/test_api.js
@@ -1,8 +1,5 @@
 process.env.NODE_ENV = 'development';
 
-var mongoose    = require('mongoose');
-var DBmodel     = require('./../../app/models/car_schema');
-
 var chai        = require('chai'),
     chaiHttp    = require('chai-http'),
     server      = require('./../../app.js'),
